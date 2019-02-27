d6c59aa817a7eb5bcd42b3c80d5e1b2383b01180
Chuan Miao <chuan137@gmail.com>
2019-02-27 08:48:35 +0100

    Init

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+var (
+	url      string
+	username string
+	password string
+)
+
+const (
+	_url    = "https://%s/servlets/netapp.servlets.admin.XMLrequest_filer"
+	version = "1.7"
+)
+
+func main() {
+
+	// vserverInfo := &netapp.VServerInfo{
+	// 	VserverName:   "1",
+	// 	UUID:          "1",
+	// 	State:         "1",
+	// 	AggregateList: &[]string{"x"},
+	// }
+
+	// volumeQuery:= &netapp.VolumeInfo{
+
+	// }
+
+	// volumeInfo := &netapp.VolumeInfo{
+	// 	VolumeIDAttributes: &netapp.VolumeIDAttributes{
+	// 		Name:              "x",
+	// 		OwningVserverName: "x",
+	// 	},
+	// }
+
+}
