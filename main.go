9fa34d9e6a6fd30e22306b5e8feb209e1b08063b
Chuan Miao <chuan137@gmail.com>
2019-05-24 18:01:00 +0200

    minor

--- main.go
+++ main.go
@@ -20,21 +20,17 @@ var (
 	configFile    = kingpin.Flag("config", "Config file").Short('c').Default("./netapp_filers.yaml").String()
 	listenAddress = kingpin.Flag("listen", "Listen address").Short('l').Default("0.0.0.0").String()
 	logger        = logrus.New()
+
+	filers []*Filer
 )
 
 type myFormatter struct{}
 
 func main() {
-	var filers []*Filer
-
 	kingpin.Parse()
 
 	logger.Out = os.Stdout
 	logger.SetFormatter(new(myFormatter))
-	// logger.SetFormatter(&logrus.TextFormatter{
-	// 	DisableColors: true,
-	// })
-
 	if os.Getenv("DEV") != "" {
 		logger.Level = logrus.DebugLevel
 		filers = loadFilerFromEnv()
@@ -42,13 +38,11 @@ func main() {
 		logger.Level = logrus.InfoLevel
 		filers = loadFilerFromFile(*configFile)
 	}
-
 	for _, f := range filers {
 		logger.Printf("Host (%s) loaded", f.Host)
 	}
 
 	p := NewCapacityExporter()
-
 	for _, f := range filers {
 		f.Init()
 		go p.runGetNetappShare(f, time.Duration(*sleepTime))
@@ -90,8 +84,7 @@ func loadFilerFromEnv() (c []*Filer) {
 func (f *myFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	s := fmt.Sprintf("%s [%s] %s\t", entry.Time.Format("2006-01-02 15:04:05.000"), entry.Level, entry.Message)
 	for k, v := range entry.Data {
-		s1 := fmt.Sprintf(" %s=%s", k, v)
-		s = s + s1
+		s = s + fmt.Sprintf(" %s=%s", k, v)
 	}
 	s = s + "\n"
 	return []byte(s), nil
