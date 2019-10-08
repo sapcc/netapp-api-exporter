011b9577ce82604f833767ca4481797415b5a5ba
Chuan Miao <chuan137@gmail.com>
2019-10-08 16:04:49 +0200

    clear compiling errors

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
+//"github.com/pepabo/go-netapp/netapp"
 import (
-	"context"
 	"fmt"
-	"github.com/pepabo/go-netapp/netapp"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -47,9 +46,7 @@ func init() {
 }
 
 func main() {
-	volumeGV := NewVolumeGaugeVec()
-	aggrGV := NewAggrGaugeVec()
-
+	// try loading filers every 5 seconds until successful
 	for {
 		filers = loadFilers()
 		if len(filers) == 0 {
@@ -59,73 +56,13 @@ func main() {
 		break
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
 	for _, f := range filers {
-		go fetchData(ctx, f)
-		go processVolumes(ctx, f, volumeGV)
-		go processAggregates(ctx, f, aggrGV)
+		fc := NewFilerCollector(f)
+		prometheus.MustRegister(fc)
 	}
 
-	prometheus.MustRegister(volumeGV)
-	prometheus.MustRegister(aggrGV)
 	http.Handle("/metrics", promhttp.Handler())
 	logger.Fatal(http.ListenAndServe(*listenAddress+":9108", nil))
-}
-
-func fetchData(ctx context.Context, f *Filer) {
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-		}
-		f.GetNetappVolume(f.volChan, f.getVolDone)
-		f.GetNetappAggregate(f.aggrChan, f.getAggrDone)
-		time.Sleep(time.Duration(*sleepTime) * time.Second)
-	}
-}
-
-func processVolumes(ctx context.Context, f *Filer, gv VolumeGaugeVec) {
-	rcvdVolumes := make(map[string]*NetappVolume)
-	volumes := make(map[string]bool)
-	for {
-		select {
-		case v := <-f.volChan:
-			logger.Debugf("[%s] Volume %s received: %s", f.Name, v.ShareID, v.SizeUsed)
-			gv.SetMetric(v)
-			rcvdVolumes[v.ShareID] = v
-			volumes[v.ShareID] = true
-		case <-f.getVolDone:
-			for shareID, ok := range volumes {
-				if !ok {
-					gv.DeleteMetric(rcvdVolumes[shareID])
-					delete(rcvdVolumes, shareID)
-					delete(volumes, shareID)
-					logger.Debugf("[%s] Volume %s deleted", f.Name, shareID)
-				}
-			}
-			for shareID, _ := range volumes {
-				volumes[shareID] = false
-			}
-		case <-ctx.Done():
-			return
-		}
-	}
-}
-
-func processAggregates(ctx context.Context, f *Filer, gv AggrGaugeVec) {
-	for {
-		select {
-		case ag := <-f.aggrChan:
-			logger.Debugf("[%s] NetappAggregate %s received", f.Name, ag.Name)
-			gv.SetMetric(ag)
-		case <-f.getAggrDone:
-		case <-ctx.Done():
-			return
-		}
-	}
 }
 
 func loadFilerFromFile(fileName string) (c []*Filer) {
@@ -184,13 +121,10 @@ func (f *myFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	return []byte(s), nil
 }
 
-//volumes    []*NetappVolume
-//aggregates []*NetappAggregate
-
 type FilerCollector struct {
 	*Filer
 	volumeList []*NetappVolume
-	mux        sync.Mutex
+	mtx        sync.Mutex
 
 	volumeSizeTotal                    *prometheus.Desc
 	volumeSizeAvail                    *prometheus.Desc
@@ -230,18 +164,18 @@ func NewFilerCollector(f *Filer) prometheus.Collector {
 		"aggregate",
 	}
 	c := &FilerCollector{
-		//Filer: f,
+		Filer: f,
 		volumeSizeTotal: prometheus.NewDesc(
 			"netapp_capacity_svm",
 			"Netapp Volume Metrics: total size",
 			volumeLabels,
 			prometheus.Labels{"metric": "size_total"},
 		),
-		aggregateSizeAvail: prometheus.NewDesc(
+		aggregateSizeTotal: prometheus.NewDesc(
 			"netapp_capacity_aggregate",
-			"Netapp Aggregate Metrics",
+			"Netapp Aggregate Metrics: total size",
 			aggregateLabels,
-			nil,
+			prometheus.Labels{"metric": "size_total"},
 		),
 		up: prometheus.NewGauge(prometheus.GaugeOpts{
 			Namespace: "netapp",
@@ -267,13 +201,13 @@ func NewFilerCollector(f *Filer) prometheus.Collector {
 	return c
 }
 
-func (c *FilerCollector) Collect(chan<- prometheus.Metric) {
+func (c *FilerCollector) Collect(ch chan<- prometheus.Metric) {
 	var err error
 	if time.Now().Sub(c.lastScrapeTime).Seconds() > *sleepTime {
 		c.lastScrapeTime = time.Now()
-		c.mux.Lock()
+		c.mtx.Lock()
 		c.volumeList, err = c.Filer.GetNetappVolume()
-		c.mux.Unlock()
+		c.mtx.Unlock()
 		if err != nil {
 			c.up.Set(0)
 			return
@@ -282,47 +216,47 @@ func (c *FilerCollector) Collect(chan<- prometheus.Metric) {
 			labels := []string{c.Filer.Name, v.Vserver, v.Volume, v.ProjectID, v.ShareID}
 
 			sizeTotal, _ := strconv.ParseFloat(v.SizeTotal, 64)
-			sizeAvailable, _ := strconv.ParseFloat(v.SizeAvailable, 64)
-			sizeUsed, _ := strconv.ParseFloat(v.SizeUsed, 64)
-			sizeUsedBySnapshots, _ := strconv.ParseFloat(v.SizeUsedBySnapshots, 64)
-			sizeAvailableForSnapshots, _ := strconv.ParseFloat(v.SizeAvailableForSnapshots, 64)
-			snapshotReserveSize, _ := strconv.ParseFloat(v.SnapshotReserveSize, 64)
-			percentageSizeUsed, _ := strconv.ParseFloat(v.PercentageSizeUsed, 64)
-			percentageCompressionSpaceSaved, _ := strconv.ParseFloat(v.PercentageCompressionSpaceSaved, 64)
-			percentageDeduplicationSpaceSaved, _ := strconv.ParseFloat(v.PercentageDeduplicationSpaceSaved, 64)
-			percentageTotalSpaceSaved, _ := strconv.ParseFloat(v.PercentageTotalSpaceSaved, 64)
+			//sizeAvailable, _ := strconv.ParseFloat(v.SizeAvailable, 64)
+			//sizeUsed, _ := strconv.ParseFloat(v.SizeUsed, 64)
+			//sizeUsedBySnapshots, _ := strconv.ParseFloat(v.SizeUsedBySnapshots, 64)
+			//sizeAvailableForSnapshots, _ := strconv.ParseFloat(v.SizeAvailableForSnapshots, 64)
+			//snapshotReserveSize, _ := strconv.ParseFloat(v.SnapshotReserveSize, 64)
+			//percentageSizeUsed, _ := strconv.ParseFloat(v.PercentageSizeUsed, 64)
+			//percentageCompressionSpaceSaved, _ := strconv.ParseFloat(v.PercentageCompressionSpaceSaved, 64)
+			//percentageDeduplicationSpaceSaved, _ := strconv.ParseFloat(v.PercentageDeduplicationSpaceSaved, 64)
+			//percentageTotalSpaceSaved, _ := strconv.ParseFloat(v.PercentageTotalSpaceSaved, 64)
 			ch <- prometheus.MustNewConstMetric(c.volumeSizeTotal, prometheus.GaugeValue, sizeTotal, labels...)
-			ch <- prometheus.MustNewConstMetric(c.volumeSizeAvail, prometheus.GaugeValue, sizeAvailable, labels...)
-			ch <- prometheus.MustNewConstMetric(c.volumeSizeUsed, prometheus.GaugeValue, sizeUsed, labels...)
-			ch <- prometheus.MustNewConstMetric(c.volumeSizeUsedBySnapshots, prometheus.GaugeValue, sizeUsedBySnapshots, labels...)
-			ch <- prometheus.MustNewConstMetric(c.volumeSizeAvailForSnapshots, prometheus.GaugeValue, sizeAvailableForSnapshots, labels...)
-			ch <- prometheus.MustNewConstMetric(c.volumeSizeReservedForSnapshots, prometheus.GaugeValue, snapshotReserveSize, labels...)
-			ch <- prometheus.MustNewConstMetric(c.volumePercentageUsed, prometheus.GaugeValue, percentageSizeUsed, labels...)
-			ch <- prometheus.MustNewConstMetric(c.volumePercentageTotalSaved, prometheus.GaugeValue, percentageTotalSpaceSaved, labels...)
-			ch <- prometheus.MustNewConstMetric(c.volumePercentageCompressionSaved, prometheus.GaugeValue, percentageCompressionSpaceSaved, labels...)
-			ch <- prometheus.MustNewConstMetric(c.volumePercentageDeduplicationSaved, prometheus.GaugeValue, percentageDeduplicationSpaceSaved, labels...)
+			//ch <- prometheus.MustNewConstMetric(c.volumeSizeAvail, prometheus.GaugeValue, sizeAvailable, labels...)
+			//ch <- prometheus.MustNewConstMetric(c.volumeSizeUsed, prometheus.GaugeValue, sizeUsed, labels...)
+			//ch <- prometheus.MustNewConstMetric(c.volumeSizeUsedBySnapshots, prometheus.GaugeValue, sizeUsedBySnapshots, labels...)
+			//ch <- prometheus.MustNewConstMetric(c.volumeSizeAvailForSnapshots, prometheus.GaugeValue, sizeAvailableForSnapshots, labels...)
+			//ch <- prometheus.MustNewConstMetric(c.volumeSizeReservedForSnapshots, prometheus.GaugeValue, snapshotReserveSize, labels...)
+			//ch <- prometheus.MustNewConstMetric(c.volumePercentageUsed, prometheus.GaugeValue, percentageSizeUsed, labels...)
+			//ch <- prometheus.MustNewConstMetric(c.volumePercentageTotalSaved, prometheus.GaugeValue, percentageTotalSpaceSaved, labels...)
+			//ch <- prometheus.MustNewConstMetric(c.volumePercentageCompressionSaved, prometheus.GaugeValue, percentageCompressionSpaceSaved, labels...)
+			//ch <- prometheus.MustNewConstMetric(c.volumePercentageDeduplicationSaved, prometheus.GaugeValue, percentageDeduplicationSpaceSaved, labels...)
 		}
 	}
 }
 
 func (c *FilerCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.volumeSizeTotal
-	ch <- c.volumeSizeAvail
-	ch <- c.volumeSizeUsed
-	ch <- c.volumeSizeUsedBySnapshots
-	ch <- c.volumeSizeAvailForSnapshots
-	ch <- c.volumeSizeReservedForSnapshots
-	ch <- c.volumePercentageUsed
-	ch <- c.volumePercentageCompressionSaved
-	ch <- c.volumePercentageDeduplicationSaved
-	ch <- c.volumePercentageTotalSaved
-	ch <- c.aggregateSizeUsed
-	ch <- c.aggregateSizeTotal
-	ch <- c.aggregateSizeAvail
-	ch <- c.aggregateSizeTotalReserved
-	ch <- c.aggregatePercentUsed
-	ch <- c.aggregatePhysicalUsed
-	ch <- c.aggregatePercentPhysicalUsed
+	//ch <- c.volumeSizeAvail
+	//ch <- c.volumeSizeUsed
+	//ch <- c.volumeSizeUsedBySnapshots
+	//ch <- c.volumeSizeAvailForSnapshots
+	//ch <- c.volumeSizeReservedForSnapshots
+	//ch <- c.volumePercentageUsed
+	//ch <- c.volumePercentageCompressionSaved
+	//ch <- c.volumePercentageDeduplicationSaved
+	//ch <- c.volumePercentageTotalSaved
+	//ch <- c.aggregateSizeUsed
+	//ch <- c.aggregateSizeTotal
+	//ch <- c.aggregateSizeAvail
+	//ch <- c.aggregateSizeTotalReserved
+	//ch <- c.aggregatePercentUsed
+	//ch <- c.aggregatePhysicalUsed
+	//ch <- c.aggregatePercentPhysicalUsed
 	ch <- c.up.Desc()
 	ch <- c.scrapesTotal.Desc()
 	ch <- c.scrapeDuration.Desc()
