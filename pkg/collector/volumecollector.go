6e18ba968c02729240a3f14b33cd44b8d12b27e8
chuan137 <chuan137@gmail.com>
2020-11-16 16:04:47 +0100

    feat(volumeCollector): add scrape metrics
    netapp_volume_scrape_total
    netapp_volume_scrape_failure_total
    netapp_volume_scrape_duration_seconds
    netapp_volume_total

--- pkg/collector/volumecollector.go
+++ pkg/collector/volumecollector.go
@@ -10,13 +10,17 @@ import (
 )
 
 type VolumeCollector struct {
-	filerName       string
-	client          *netapp.Client
-	metrics         []VolumeMetric
-	volumes         []*netapp.Volume
-	mux             sync.Mutex
-	retentionPeriod time.Duration
-	errorCh         chan<- error
+	filerName            string
+	client               *netapp.Client
+	volumes              []*netapp.Volume
+	volumeMetrics        []VolumeMetric
+	volumeTotalGauge     prometheus.Gauge
+	scrapeCounter        prometheus.Counter
+	scrapeFailureCounter prometheus.Counter
+	scrapeDurationGauge  prometheus.Gauge
+	mux                  sync.Mutex
+	retentionPeriod      time.Duration
+	errorCh              chan<- error
 }
 
 type VolumeMetric struct {
@@ -27,109 +31,142 @@ type VolumeMetric struct {
 
 func NewVolumeCollector(filerName string, client *netapp.Client, ch chan<- error, retentionPeriod time.Duration) *VolumeCollector {
 	volumeLabels := []string{"vserver", "volume", "project_id", "share_id", "share_name", "share_type"}
-	return &VolumeCollector{
-		filerName:       filerName,
-		client:          client,
-		errorCh:         ch,
-		retentionPeriod: retentionPeriod,
-		metrics: []VolumeMetric{
-			{
-				desc: prometheus.NewDesc(
-					"netapp_volume_state",
-					"Netapp Volume Metrics: state (1: online; 2: restricted; 3: offline; 4: quiesced)",
-					volumeLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(v *netapp.Volume) float64 { return float64(v.State) },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_volume_total_bytes",
-					"Netapp Volume Metrics: total size",
-					volumeLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(v *netapp.Volume) float64 { return v.SizeTotal },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_volume_used_bytes",
-					"Netapp Volume Metrics: used size",
-					volumeLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(v *netapp.Volume) float64 { return v.SizeUsed },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_volume_available_bytes",
-					"Netapp Volume Metrics: available size",
-					volumeLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(v *netapp.Volume) float64 { return v.SizeAvailable },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_volume_snapshot_used_bytes",
-					"Netapp Volume Metrics: size used by snapshots",
-					volumeLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(v *netapp.Volume) float64 { return v.SizeUsedBySnapshots },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_volume_snapshot_available_bytes",
-					"Netapp Volume Metrics: size available for snapshots",
-					volumeLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(v *netapp.Volume) float64 { return v.SizeAvailableForSnapshots },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_volume_snapshot_reserved_bytes",
-					"Netapp Volume Metrics: size reserved for snapshots",
-					volumeLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(v *netapp.Volume) float64 { return v.SnapshotReserveSize },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_volume_used_percentage",
-					"Netapp Volume Metrics: used percentage ",
-					volumeLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(v *netapp.Volume) float64 { return v.PercentageSizeUsed },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_volume_saved_total_percentage",
-					"Netapp Volume Metrics: percentage of space compression and deduplication saved",
-					volumeLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(v *netapp.Volume) float64 { return v.PercentageTotalSpaceSaved },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_volume_saved_compression_percentage",
-					"Netapp Volume Metrics: percentage of space compression saved",
-					volumeLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(v *netapp.Volume) float64 { return v.PercentageCompressionSpaceSaved },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_volume_saved_deduplication_percentage",
-					"Netapp Volume Metrics: percentage of space deduplication saved",
-					volumeLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(v *netapp.Volume) float64 { return v.PercentageDeduplicationSpaceSaved },
-			},
+	volumeMetrics := []VolumeMetric{
+		{
+			desc: prometheus.NewDesc(
+				"netapp_volume_state",
+				"Netapp Volume Metrics: state (1: online; 2: restricted; 3: offline; 4: quiesced)",
+				volumeLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(v *netapp.Volume) float64 { return float64(v.State) },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_volume_total_bytes",
+				"Netapp Volume Metrics: total size",
+				volumeLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(v *netapp.Volume) float64 { return v.SizeTotal },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_volume_used_bytes",
+				"Netapp Volume Metrics: used size",
+				volumeLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(v *netapp.Volume) float64 { return v.SizeUsed },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_volume_available_bytes",
+				"Netapp Volume Metrics: available size",
+				volumeLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(v *netapp.Volume) float64 { return v.SizeAvailable },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_volume_snapshot_used_bytes",
+				"Netapp Volume Metrics: size used by snapshots",
+				volumeLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(v *netapp.Volume) float64 { return v.SizeUsedBySnapshots },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_volume_snapshot_available_bytes",
+				"Netapp Volume Metrics: size available for snapshots",
+				volumeLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(v *netapp.Volume) float64 { return v.SizeAvailableForSnapshots },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_volume_snapshot_reserved_bytes",
+				"Netapp Volume Metrics: size reserved for snapshots",
+				volumeLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(v *netapp.Volume) float64 { return v.SnapshotReserveSize },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_volume_used_percentage",
+				"Netapp Volume Metrics: used percentage ",
+				volumeLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(v *netapp.Volume) float64 { return v.PercentageSizeUsed },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_volume_saved_total_percentage",
+				"Netapp Volume Metrics: percentage of space compression and deduplication saved",
+				volumeLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(v *netapp.Volume) float64 { return v.PercentageTotalSpaceSaved },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_volume_saved_compression_percentage",
+				"Netapp Volume Metrics: percentage of space compression saved",
+				volumeLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(v *netapp.Volume) float64 { return v.PercentageCompressionSpaceSaved },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_volume_saved_deduplication_percentage",
+				"Netapp Volume Metrics: percentage of space deduplication saved",
+				volumeLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(v *netapp.Volume) float64 { return v.PercentageDeduplicationSpaceSaved },
 		},
+	}
+	volumeTotalGauge := prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "netapp_volume_total",
+			Help: "number of volumes scraped from Netapp filer",
+		},
+	)
+	scrapeDurationGauge := prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "netapp_volume_scrape_duration_seconds",
+			Help: "duration in seconds used to fetch volumes from filer",
+		},
+	)
+	scrapeCounter := prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "netapp_volume_scrape_total",
+			Help: "number of volume fetches from filer",
+		},
+	)
+	scrapeFailureCounter := prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "netapp_volume_scrape_failure_total",
+			Help: "number of failures for fetching volumes from filer",
+		},
+	)
+	return &VolumeCollector{
+		filerName:            filerName,
+		client:               client,
+		errorCh:              ch,
+		retentionPeriod:      retentionPeriod,
+		volumeMetrics:        volumeMetrics,
+		volumeTotalGauge:     volumeTotalGauge,
+		scrapeCounter:        scrapeCounter,
+		scrapeFailureCounter: scrapeFailureCounter,
+		scrapeDurationGauge:  scrapeDurationGauge,
 	}
 }
 
 func (c *VolumeCollector) Describe(ch chan<- *prometheus.Desc) {
-	for _, m := range c.metrics {
+	for _, m := range c.volumeMetrics {
 		ch <- m.desc
 	}
+	ch <- c.volumeTotalGauge.Desc()
+	ch <- c.scrapeCounter.Desc()
+	ch <- c.scrapeFailureCounter.Desc()
+	ch <- c.scrapeDurationGauge.Desc()
 }
 
 func (c *VolumeCollector) Collect(ch chan<- prometheus.Metric) {
@@ -139,12 +176,14 @@ func (c *VolumeCollector) Collect(ch chan<- prometheus.Metric) {
 	// fetch volumes
 	if c.volumes == nil {
 		c.volumes = c.Fetch()
+		c.volumeTotalGauge.Set(float64(len(c.volumes)))
 		if len(c.volumes) > 0 {
 			time.AfterFunc(c.retentionPeriod, func() {
 				defer c.mux.Unlock()
 				c.mux.Lock()
 				log.Debugf("VolumeCollector[%v] cached volumes cleared", c.filerName)
 				c.volumes = nil
+				c.volumeTotalGauge.Set(0)
 			})
 		}
 	}
@@ -153,18 +192,27 @@ func (c *VolumeCollector) Collect(ch chan<- prometheus.Metric) {
 	log.Debugf("VolumeCollector[%v] Collect() exporting %d volumes", c.filerName, len(c.volumes))
 	for _, volume := range c.volumes {
 		volumeLabels := []string{volume.Vserver, volume.Volume, volume.ProjectID, volume.ShareID, volume.ShareName, volume.ShareType}
-		for _, m := range c.metrics {
+		for _, m := range c.volumeMetrics {
 			ch <- prometheus.MustNewConstMetric(m.desc, m.valueType, m.getterFn(volume), volumeLabels...)
 		}
 	}
+	c.volumeTotalGauge.Collect(ch)
+	c.scrapeCounter.Collect(ch)
+	c.scrapeFailureCounter.Collect(ch)
+	c.scrapeDurationGauge.Collect(ch)
 	return
 }
 
 func (c *VolumeCollector) Fetch() []*netapp.Volume {
 	log.Debugf("VolumeCollector[%v] starts fetching volumes", c.filerName)
+	start := time.Now()
 	volumes, err := c.client.ListVolumes()
+	elapsed := time.Now().Sub(start)
+	c.scrapeCounter.Inc()
+	c.scrapeDurationGauge.Set(elapsed.Seconds())
 	if err != nil {
 		log.Error(err)
+		c.scrapeFailureCounter.Inc()
 		return nil
 	}
 	log.Debugf("VolumeCollector[%v] fetched %d volumes", c.filerName, len(volumes))
