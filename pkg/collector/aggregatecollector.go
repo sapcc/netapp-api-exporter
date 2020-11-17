817f4c35aa10682db06a0a5f095940d9b2ec9012
chuan137 <chuan137@gmail.com>
2020-11-17 17:12:21 +0100

    feat(aggrgateCollector) add scrape metrics
    follow up of 6e18ba968c02729240a3f14b33cd44b8d12b27e8
    
    netapp_aggregate_scrape_total
    netapp_aggregate_scrape_failure_total
    netapp_aggregate_scrape_duration_seconds

--- pkg/collector/aggregatecollector.go
+++ pkg/collector/aggregatecollector.go
@@ -1,7 +1,6 @@
 package collector
 
 import (
-	"sync"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -10,13 +9,13 @@ import (
 )
 
 type AggregateCollector struct {
-	filerName       string
-	client          *netapp.Client
-	metrics         []AggregateMetric
-	aggregates      []*netapp.Aggregate
-	mux             sync.Mutex
-	retentionPeriod time.Duration
-	errorCh         chan<- error
+	filerName            string
+	client               *netapp.Client
+	aggregateMetrics     []AggregateMetric
+	scrapeCounter        prometheus.Counter
+	scrapeFailureCounter prometheus.Counter
+	scrapeDurationGauge  prometheus.Gauge
+	errorCh              chan<- error
 }
 
 type AggregateMetric struct {
@@ -25,101 +24,123 @@ type AggregateMetric struct {
 	getterFn  func(aggr *netapp.Aggregate) float64
 }
 
-func NewAggregateCollector(filerName string, client *netapp.Client, ch chan<- error, retentionPeriod time.Duration) *AggregateCollector {
+func NewAggregateCollector(filerName string, client *netapp.Client, ch chan<- error, fetchPeriod time.Duration) *AggregateCollector {
 	aggrLabels := []string{"node", "aggregate"}
-	return &AggregateCollector{
-		filerName:       filerName,
-		client:          client,
-		errorCh:         ch,
-		retentionPeriod: retentionPeriod,
-		metrics: []AggregateMetric{
-			{
-				desc: prometheus.NewDesc(
-					"netapp_aggregate_total_bytes",
-					"Netapp Aggregate Metrics: total size",
-					aggrLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(m *netapp.Aggregate) float64 { return m.SizeTotal },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_aggregate_available_bytes",
-					"Netapp Aggregate Metrics: available size",
-					aggrLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(m *netapp.Aggregate) float64 { return m.SizeAvailable },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_aggregate_used_bytes",
-					"Netapp Aggregate Metrics: used size",
-					aggrLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(m *netapp.Aggregate) float64 { return m.SizeUsed },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_aggregate_used_percentage",
-					"Netapp Aggregate Metrics: used percentage",
-					aggrLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(m *netapp.Aggregate) float64 { return m.PercentUsedCapacity },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_aggregate_physical_used_bytes",
-					"Netapp Aggregate Metrics: physical used size",
-					aggrLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(m *netapp.Aggregate) float64 { return m.PhysicalUsed },
-			}, {
-				desc: prometheus.NewDesc(
-					"netapp_aggregate_physical_used_percentage",
-					"Netapp Aggregate Metrics: physical used percentage",
-					aggrLabels,
-					nil),
-				valueType: prometheus.GaugeValue,
-				getterFn:  func(m *netapp.Aggregate) float64 { return m.PhysicalUsedPercent },
-			},
+	aggrMetrics := []AggregateMetric{
+		{
+			desc: prometheus.NewDesc(
+				"netapp_aggregate_total_bytes",
+				"Netapp Aggregate Metrics: total size",
+				aggrLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(m *netapp.Aggregate) float64 { return m.SizeTotal },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_aggregate_available_bytes",
+				"Netapp Aggregate Metrics: available size",
+				aggrLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(m *netapp.Aggregate) float64 { return m.SizeAvailable },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_aggregate_used_bytes",
+				"Netapp Aggregate Metrics: used size",
+				aggrLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(m *netapp.Aggregate) float64 { return m.SizeUsed },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_aggregate_used_percentage",
+				"Netapp Aggregate Metrics: used percentage",
+				aggrLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(m *netapp.Aggregate) float64 { return m.PercentUsedCapacity },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_aggregate_physical_used_bytes",
+				"Netapp Aggregate Metrics: physical used size",
+				aggrLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(m *netapp.Aggregate) float64 { return m.PhysicalUsed },
+		}, {
+			desc: prometheus.NewDesc(
+				"netapp_aggregate_physical_used_percentage",
+				"Netapp Aggregate Metrics: physical used percentage",
+				aggrLabels,
+				nil),
+			valueType: prometheus.GaugeValue,
+			getterFn:  func(m *netapp.Aggregate) float64 { return m.PhysicalUsedPercent },
 		},
+	}
+	scrapeDurationGauge := prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "netapp_aggregate_scrape_duration_seconds",
+			Help: "duration in seconds of fetching aggregates from filer",
+		},
+	)
+	scrapeCounter := prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "netapp_aggregate_scrape_total",
+			Help: "number of aggregate fetches from filer",
+		},
+	)
+	scrapeFailureCounter := prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "netapp_aggregate_scrape_failure_total",
+			Help: "number of failures for fetching aggregates from filer",
+		},
+	)
+	return &AggregateCollector{
+		filerName:            filerName,
+		client:               client,
+		errorCh:              ch,
+		aggregateMetrics:     aggrMetrics,
+		scrapeDurationGauge:  scrapeDurationGauge,
+		scrapeCounter:        scrapeCounter,
+		scrapeFailureCounter: scrapeFailureCounter,
 	}
 }
 
 func (c *AggregateCollector) Describe(ch chan<- *prometheus.Desc) {
-	for _, m := range c.metrics {
+	for _, m := range c.aggregateMetrics {
 		ch <- m.desc
 	}
+	ch <- c.scrapeCounter.Desc()
+	ch <- c.scrapeFailureCounter.Desc()
+	ch <- c.scrapeDurationGauge.Desc()
 }
 
 func (c *AggregateCollector) Collect(ch chan<- prometheus.Metric) {
-	defer c.mux.Unlock()
-	c.mux.Lock()
-	if c.aggregates == nil {
-		if err := c.Fetch(); err != nil {
-			log.Error(err)
-			c.errorCh <- err
-			return
-		}
-	}
-	for _, aggr := range c.aggregates {
+	// fetch aggregates
+	aggregates := c.Fetch()
+
+	// export metrics
+	for _, aggr := range aggregates {
 		labels := []string{aggr.OwnerName, aggr.Name}
-		for _, m := range c.metrics {
+		for _, m := range c.aggregateMetrics {
 			ch <- prometheus.MustNewConstMetric(m.desc, m.valueType, m.getterFn(aggr), labels...)
 		}
 	}
+	c.scrapeCounter.Collect(ch)
+	c.scrapeFailureCounter.Collect(ch)
+	c.scrapeDurationGauge.Collect(ch)
 }
 
-func (c *AggregateCollector) Fetch() error {
+func (c *AggregateCollector) Fetch() []*netapp.Aggregate {
+	start := time.Now()
 	aggregates, err := c.client.ListAggregates()
+	elapsed := time.Now().Sub(start)
+	c.scrapeCounter.Inc()
+	c.scrapeDurationGauge.Set(elapsed.Seconds())
 	if err != nil {
-		return err
+		log.Error(err)
+		c.scrapeFailureCounter.Inc()
+		return nil
 	}
-	c.aggregates = aggregates
-	time.AfterFunc(c.retentionPeriod, func() {
-		defer c.mux.Unlock()
-		c.mux.Lock()
-		c.aggregates = nil
-	})
-	return nil
+	return aggregates
 }
