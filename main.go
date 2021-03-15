25b3550bbd29e6a14866c33b4ee1fbc6a88d1e33
chuan137 <chuan137@gmail.com>
2021-03-15 15:36:13 +0100

    Load and register new filer on the fly

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -24,7 +25,86 @@ var (
 	disableSystem     = kingpin.Flag("no-system", "Disable system collector").Bool()
 )
 
-type logFormatter struct{}
+func main() {
+	var filers map[string]Filer
+
+	// new prometheus exporter registry and register go process collector
+	reg := prometheus.NewPedanticRegistry()
+	reg.MustRegister(prometheus.NewGoCollector())
+
+	// load filers from configuration every minute and register new colloector
+	// for new filer
+	go func() {
+		ticker := time.NewTicker(1 * time.Minute)
+
+		for {
+			ff, err := loadFilers(*configFile)
+			if err != nil {
+				log.WithField("File", *configFile).Errorf("failed to load configuration: %v", err)
+				return
+			} else {
+				// register collector for new filer
+				for _, f := range ff {
+					if _, ok := filers[f.Host]; !ok {
+						err = registerFiler(reg, f)
+						if err != nil {
+							log.Error(err)
+						}
+						if filers == nil {
+							filers = make(map[string]Filer)
+						}
+						filers[f.Host] = f
+					}
+				}
+			}
+
+			select {
+			case <-ticker.C:
+			}
+		}
+	}()
+
+	port := "9108"
+	addr := *listenAddress + ":" + port
+	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+	log.Debugf("open link http://%s/metrics for metrics", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func registerFiler(reg prometheus.Registerer, f Filer) error {
+	if f.Name == "" {
+		return fmt.Errorf("Filer.Name is not set ")
+	}
+	if f.AvailabilityZone == "" {
+		return fmt.Errorf("Filer.AvailabilityZone is not set ")
+	}
+	log.WithFields(log.Fields{
+		"Name":      f.Name,
+		"Host":      f.Host,
+		"AZ":        f.AvailabilityZone,
+		"Aggregate": f.AggregatePattern,
+	}).Info("register filer")
+	extraLabels := prometheus.Labels{
+		"filer":             f.Name,
+		"availability_zone": f.AvailabilityZone,
+	}
+	prometheus.WrapRegistererWith(extraLabels, reg).MustRegister(f.ScrapeFailures)
+	prometheus.WrapRegistererWith(extraLabels, reg).MustRegister(f.FilerDNSFailures)
+	prometheus.WrapRegistererWith(extraLabels, reg).MustRegister(f.FilerTimeoutFailures)
+	if !*disableAggregate {
+		prometheus.WrapRegistererWith(extraLabels, reg).MustRegister(
+			collector.NewAggregateCollector(f.Client, f.Name, f.AggregatePattern))
+	}
+	if !*disableVolume {
+		prometheus.WrapRegistererWith(extraLabels, reg).MustRegister(
+			collector.NewVolumeCollector(f.Client, f.Name, *volumeFetchPeriod))
+	}
+	if !*disableSystem {
+		prometheus.WrapRegistererWith(extraLabels, reg).MustRegister(
+			collector.NewSystemCollector(f.Client, f.Name))
+	}
+	return nil
+}
 
 func init() {
 	kingpin.Parse()
@@ -32,64 +112,20 @@ func init() {
 	log.SetOutput(os.Stdout)
 	log.SetFormatter(new(logFormatter))
 	if *debug {
-		log.Info("Debug mode")
+		log.Info("debug mode")
 		log.SetLevel(log.DebugLevel)
 	} else {
 		log.SetLevel(log.InfoLevel)
 	}
 }
 
-func main() {
-	// try loading filers every  10 seconds until successful
-	var filers []Filer
-	var err error
-	for {
-		filers, err = loadFilers(*configFile)
-		if err != nil {
-			log.Errorf("Failed to load filer configuration: %v. Retry in 10 seconds...", err)
-			time.Sleep(10 * time.Second)
-			continue
-		}
-		break
-	}
-
-	reg := prometheus.NewPedanticRegistry()
-
-	// register go process collector
-	reg.MustRegister(prometheus.NewGoCollector())
-
-	for _, f := range filers {
-		extraLabels := prometheus.Labels{
-			"filer":             f.Name,
-			"availability_zone": f.AvailabilityZone,
-		}
-		log.Infof("Register collectors for filer: {Name=%s, Host=%s, Username=%s}", f.Name, f.Host, f.Username)
-		prometheus.WrapRegistererWith(extraLabels, reg).MustRegister(f.ScrapeFailures)
-		prometheus.WrapRegistererWith(extraLabels, reg).MustRegister(f.FilerDNSFailures)
-		prometheus.WrapRegistererWith(extraLabels, reg).MustRegister(f.FilerTimeoutFailures)
-		if !*disableAggregate {
-			prometheus.WrapRegistererWith(extraLabels, reg).MustRegister(
-				collector.NewAggregateCollector(f.Client, f.Name, f.AggregatePattern))
-		}
-		if !*disableVolume {
-			prometheus.WrapRegistererWith(extraLabels, reg).MustRegister(
-				collector.NewVolumeCollector(f.Client, f.Name, *volumeFetchPeriod))
-		}
-		if !*disableSystem {
-			prometheus.WrapRegistererWith(extraLabels, reg).MustRegister(
-				collector.NewSystemCollector(f.Client, f.Name))
-		}
-	}
-
-	port := "9108"
-	addr := *listenAddress + ":" + port
-	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
-	log.Debugf("Open link http://%s/metrics for metrics", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
-}
+type logFormatter struct{}
 
 func (f *logFormatter) Format(entry *log.Entry) ([]byte, error) {
-	s := fmt.Sprintf("%s [%s] %s\t", entry.Time.Format("2006-01-02 15:04:05.000"), entry.Level, entry.Message)
+	s := fmt.Sprintf("%s %-5v msg=%q",
+		entry.Time.Format("2006-01-02 15:04:05.000"),
+		strings.ToUpper(entry.Level.String()),
+		entry.Message)
 	for k, v := range entry.Data {
 		s = s + fmt.Sprintf(" %s=%s", k, v)
 	}
